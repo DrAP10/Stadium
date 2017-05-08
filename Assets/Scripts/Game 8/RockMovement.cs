a276748e76731971b2af4fd74b1426e6fc0ed343
DrAP <agus10pg@gmail.com>
2017-05-09 01:07:31 +0200

    Change Game 8 RockMovement

--- Assets/Scripts/Game 8/RockMovement.cs
+++ Assets/Scripts/Game 8/RockMovement.cs
@@ -2,27 +2,31 @@
 using System.Collections;
 
 public class RockMovement : MonoBehaviour {
-    //float z, speed_h, time, y, aceleration,yo,zo;
-    //float speed_v = 2.5f;
-    float angle, speed, radius;
-    Vector3 origen;
+    float z, speed_h, time, y, aceleration,yo,zo;
+    float speed_v = 2.5f;
+
+    public ParticleSystem particleSystem;
+
+    /*float angle, speed, radius;
+    Vector3 origen;*/
+
     // Use this for initialization
-	void Start () {
-        /*aceleration = -60;
-        speed_h = -16;
+    void Start () {
+        aceleration = -60;
+        speed_h = -32;
         speed_v = 60;
         time = 0f;
         zo = gameObject.GetComponent<Rigidbody>().position.z;
-        yo = gameObject.GetComponent<Rigidbody>().position.y;*/
-        origen = new Vector3(0f, 2f, 16f);
+        yo = gameObject.GetComponent<Rigidbody>().position.y;
+        /*origen = new Vector3(0f, 2f, 16f);
         angle = Mathf.PI/2;
         radius = 16f;
-        speed = Mathf.PI / 2;
+        speed = Mathf.PI / 2;*/
     }
 	
 	// Update is called once per frame
 	void Update () {
-        /*time += Time.deltaTime;
+        time += Time.deltaTime;
         Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
         z = zo + speed_h * time;
         //Debug.Log("z: " + z + ", v: " + speed_h + ", t: " + time);
@@ -31,13 +35,13 @@ public class RockMovement : MonoBehaviour {
        // Debug.Log("y: " + y + ", v: " + speed_v + ", t: " + time);
         rigidbody.MovePosition(new Vector3(rigidbody.position.x, y, z));
         if(y<0f)
-            Destroy(gameObject);*/
-        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+            Destroy(gameObject);
+        /*Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
         angle -= speed * Time.deltaTime;
         float y = (Mathf.Cos(angle) * radius) + origen.y;
         float z = (Mathf.Sin(angle) * radius) + origen.z;
         //transform.Translate(new Vector3(x - transform.position.x, 0f, z - transform.position.z));
-        rigidbody.MovePosition(new Vector3(rigidbody.position.x, y, z));
+        rigidbody.MovePosition(new Vector3(rigidbody.position.x, y, z));*/
     }
 
     void OnCollisionEnter(Collision collision)
@@ -47,13 +51,16 @@ public class RockMovement : MonoBehaviour {
             if (collision.gameObject.GetComponent<G8PlayerController>().harden)
             {
                 Destroy(gameObject);
+                ParticleSystem ps = Instantiate(particleSystem, collision.transform.position, particleSystem.transform.localRotation) as ParticleSystem;
+                ps.loop = false;
+                Destroy(ps, 1);
             }
             else
             {
-                angle = Mathf.PI / 2;
+                /*angle = Mathf.PI / 2;
                 origen = new Vector3(origen.x, origen.y, -2);
                 radius = 2;
-                speed = Mathf.PI / 0.25f;
+                speed = Mathf.PI / 0.25f;*/
                 collision.gameObject.GetComponent<Animation>().Play("Impact");
                 collision.gameObject.GetComponent<G8PlayerController>().TakeDamage(10f);
                 //Debug.Log(z-time);
@@ -61,7 +68,7 @@ public class RockMovement : MonoBehaviour {
         }
         if (collision.transform.name.Equals("Plane"))
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
     }
 }
