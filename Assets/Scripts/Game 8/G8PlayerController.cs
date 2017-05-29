45587f048bef0db74d54e360fe23eaaeca923de6
DrAP <agus10pg@gmail.com>
2017-05-29 15:44:29 +0200

    Game 8 ComPlayer(without AI) and PostGame, Change InGameMenu prefab

--- Assets/Scripts/Game 8/G8PlayerController.cs
+++ Assets/Scripts/Game 8/G8PlayerController.cs
@@ -5,10 +5,13 @@ public class G8PlayerController : MonoBehaviour {
     public Material gold;
     public Material color;
     public bool harden;
-    bool dead;
+    public bool dead;
     public const float maxHealth = 100;
     public float currentHealth = maxHealth;
     public RectTransform healthBar;
+
+	public int id;
+	public bool comPlayer;
 
     public void TakeDamage(float amount)
     {
@@ -26,6 +29,26 @@ public class G8PlayerController : MonoBehaviour {
                 Destroy(rock);
             }
             gameObject.GetComponent<Animation>().Play("Dead",PlayMode.StopAll);
+
+			int idWinner=-1;
+			int playersAlive=0;
+			foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+			{
+				if (!player.GetComponent<G8PlayerController> ().dead) 
+				{
+					playersAlive++;
+					idWinner = player.GetComponent<G8PlayerController> ().id;
+				}
+			}
+			if (playersAlive==1)
+			{
+				bool[] winners = new bool[4];
+				for (int i = 0; i < 4; i++) 
+				{
+					winners [i] = i == idWinner;
+				}
+				GameObject.FindGameObjectWithTag("InGameMenu").GetComponent<PostGameScript> ().Winner (winners);
+			}
         }
 
         healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
@@ -33,27 +56,30 @@ public class G8PlayerController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         harden = false;
-        dead = false;
+		dead = false;
+		comPlayer = !GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>().players[id];
 	}
 	
 	// Update is called once per frame
 	void Update ()
-    {
-        if (dead || Time.timeScale == 0)
-            return;
-        if (Input.GetButton(transform.parent.name+" Main"))
-        {
-            if(!harden && !gameObject.GetComponent<Animation>().IsPlaying("Impact"))
-            {
-                gameObject.GetComponent<Renderer>().material = gold;
-                harden = true;
-            }
-            TakeDamage(Time.deltaTime * 10);
-        }
-        if (Input.GetButtonUp(transform.parent.name + " Main") &&harden)
-        {
-            gameObject.GetComponent<Renderer>().material = color;
-            harden = false;
-        }
-    }
+	{
+		if (dead || Time.timeScale == 0)
+			return;
+		if (!comPlayer) {
+			if (Input.GetButton (transform.parent.name + " Main")) {
+				if (!harden && !gameObject.GetComponent<Animation> ().IsPlaying ("Impact")) {
+					gameObject.GetComponent<Renderer> ().material = gold;
+					harden = true;
+				}
+				TakeDamage (Time.deltaTime * 10);
+			}
+			if (Input.GetButtonUp (transform.parent.name + " Main") && harden) {
+				gameObject.GetComponent<Renderer> ().material = color;
+				harden = false;
+			}
+		} else 
+		{
+			//IA
+		}
+	}
 }
